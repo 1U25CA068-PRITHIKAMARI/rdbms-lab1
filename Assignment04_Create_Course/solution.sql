5c0507ab64fa503466b0dfaf6df0af47830ed628
1U25CA068-PRITHIKAMARI <m.prithikamari2225@gmail.com>
2026-07-17 12:16:19 +0530

    Update solution.sql

--- Assignment04_Create_Course/solution.sql
+++ Assignment04_Create_Course/solution.sql
@@ -1,5 +1,14 @@
--- Create Course table
+CREATE TABLE Course (
+    CourseID INT PRIMARY KEY,
+    CourseName VARCHAR(100),
+    Credits INT,
+    DepartmentID INT
+);
 
--- Insert three records
-
--- Display structure
+INSERT INTO Course (CourseID, CourseName, Credits, DepartmentID) 
+VALUES 
+    (101, 'Programming', 4, 1),
+    (102, 'Web Development', 3, 1),
+    (103, 'Database Management systems', 4, 2);
+DESCRIBE Course;
+SELECT * FROM Course;
